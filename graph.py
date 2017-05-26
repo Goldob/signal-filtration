acc45aa8c96f500d86d5e4bedd07281539a52476
Adam Gotlib <gotlib.adam+dev@gmail.com>
2017-05-26 11:19:07 +0200

    Dodaj opisy osi do programu graph.py

--- graph.py
+++ graph.py
@@ -7,11 +7,16 @@ in_filename = sys.argv[1]
 in_file = open(in_filename, 'r')
 
 first_line = in_file.readline().split()
+sampling_freq = float(first_line[0])
 n_samples = int(first_line[1])
 samples = list()
 
 for i in range(n_samples):
     samples.append(float(in_file.readline()))
 
-plt.plot(samples)
+time = [i / sampling_freq for i in range(n_samples)]
+    
+plt.plot(time, samples)
+plt.xlabel('czas [s]')
+plt.ylabel('amplituda')
 plt.show()
