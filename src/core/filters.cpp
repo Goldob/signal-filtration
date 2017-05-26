9dece4956568dfca1971a9d0f9080cf1038e3c9f
Adam Gotlib <gotlib.adam+dev@gmail.com>
2017-05-26 07:29:57 +0200

    Wykonuj filtrację sygnału

--- src/core/filters.cpp
+++ src/core/filters.cpp
@@ -2,13 +2,22 @@
 #include "fourier.h"
 
 namespace sig {
+    inline int freqSampleNumber(signal_freqDomain signal_freq, freq desired_freq) {
+        return signal_freq.n * 2 * desired_freq / signal_freq.samplingFreq;
+    }
+
   void lowPassFilter (const signal in_signal,
                       const freq freq_upperBound,
                       signal& out_signal) {
       sig::signal_freqDomain signal_freq;
       sig::fourierTransform(in_signal, signal_freq);
 
-      // TODO
+      int upperK = freqSampleNumber(signal_freq, freq_upperBound);
+
+      for (int k = upperK; k < signal_freq.n; k++) {
+          signal_freq.realPart[k] = 0;
+          signal_freq.imPart[k] = 0;
+      }
 
       sig::inverseFourierTransform(signal_freq, out_signal);
   }
@@ -19,7 +28,12 @@ namespace sig {
       sig::signal_freqDomain signal_freq;
       sig::fourierTransform(in_signal, signal_freq);
 
-      // TODO
+      int lowerK = freqSampleNumber(signal_freq, freq_lowerBound);
+
+      for (int k = 0; k < std::min(lowerK, signal_freq.n); k++) {
+          signal_freq.realPart[k] = 0;
+          signal_freq.imPart[k] = 0;
+      }
 
       sig::inverseFourierTransform(signal_freq, out_signal);
   }
@@ -31,7 +45,19 @@ namespace sig {
       sig::signal_freqDomain signal_freq;
       sig::fourierTransform(in_signal, signal_freq);
 
-      // TODO
+      int lowerK = freqSampleNumber(signal_freq, freq_lowerBound);
+
+      for (int k = 0; k < std::min(lowerK, signal_freq.n); k++) {
+          signal_freq.realPart[k] = 0;
+          signal_freq.imPart[k] = 0;
+      }
+
+      int upperK = freqSampleNumber(signal_freq, freq_upperBound);
+
+      for (int k = upperK; k < signal_freq.n; k++) {
+          signal_freq.realPart[k] = 0;
+          signal_freq.imPart[k] = 0;
+      }
 
       sig::inverseFourierTransform(signal_freq, out_signal);
   }
@@ -43,7 +69,13 @@ namespace sig {
       sig::signal_freqDomain signal_freq;
       sig::fourierTransform(in_signal, signal_freq);
 
-      // TODO
+      int lowerK = freqSampleNumber(signal_freq, freq_lowerBound);
+      int upperK = freqSampleNumber(signal_freq, freq_upperBound);
+
+      for (int k = lowerK; k < std::min(upperK, signal_freq.n); k++) {
+          signal_freq.realPart[k] = 0;
+          signal_freq.imPart[k] = 0;
+      }
 
       sig::inverseFourierTransform(signal_freq, out_signal);
   }
