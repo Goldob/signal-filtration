974c0b721ad384df00980e591be213cc1e14dc1b
Adam Gotlib <gotlib.adam+dev@gmail.com>
2017-06-04 17:18:46 +0200

    Wyświetlaj osie na wykresie sygnału

--- src/gui/widgets/filter_preview.cpp
+++ src/gui/widgets/filter_preview.cpp
@@ -1,6 +1,9 @@
 #define INPUT_SIGNAL tr("Sygnał wejściowy")
 #define OUTPUT_SIGNAL tr("Sygnał wyjściowy")
 
+#define X_AXIS_LABEL tr("Czas [s]")
+#define Y_AXIS_LABEL tr("Amplituda")
+
 #include "filter_preview.h"
 
 FilterPreview::FilterPreview (dsp::signal input, QWidget * parent) :
@@ -17,15 +20,12 @@ FilterPreview::FilterPreview (dsp::signal input, QWidget * parent) :
 
     m_chart->addSeries(m_inputSeries);
     m_chart->addSeries(m_outputSeries);
+    m_chart->createDefaultAxes();
+    m_chart->axisX()->setTitleText(X_AXIS_LABEL);
+    m_chart->axisY()->setTitleText(Y_AXIS_LABEL);
 
     updateOutput(input);
     setRenderHint(QPainter::Antialiasing);
-}
-
-FilterPreview::~FilterPreview () {
-    delete m_chart;
-    delete m_inputSeries;
-    delete m_outputSeries;
 }
 
 void FilterPreview::updateOutput (dsp::signal output) {
@@ -37,6 +37,6 @@ void FilterPreview::encodeSignal (dsp::signal signal, QLineSeries * series) {
 
     int sample_num = 0;
     signal.samples.forEach([&](dsp::sample s) {
-        series->append(sample_num++, s);
+        series->append((sample_num++)/signal.samplingFreq, s);
     });
 }
